c7e6f0376fd0fb23d5f677fbbd1ebf63956f7df9
Restart <torkian@pdx.edu>
2016-01-17 02:50:09 -0800

    Changed banner.

--- app/src/main/java/com/restart/twitchgroup/SocialMedia.java
+++ app/src/main/java/com/restart/twitchgroup/SocialMedia.java
@@ -4,6 +4,7 @@ package com.restart.twitchgroup;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TabHost;
 
 public class SocialMedia extends TabActivity {
@@ -17,22 +18,26 @@ public class SocialMedia extends TabActivity {
         Intent intent;
 
         intent = new Intent().setClass(this, First_Media.class);
-        spec = tabHost.newTabSpec("Facebook").setIndicator("Facebook")
+        spec = tabHost.newTabSpec("Facebook")
+                .setIndicator("", getResources().getDrawable(R.drawable.ic_action_facebook))
                 .setContent(intent);
         tabHost.addTab(spec);
 
         intent = new Intent().setClass(this, Second_Media.class);
-        spec = tabHost.newTabSpec("Twitter").setIndicator("Twitter")
+        spec = tabHost.newTabSpec("Twitter")
+                .setIndicator("", getResources().getDrawable(R.drawable.ic_action_twitter))
                 .setContent(intent);
         tabHost.addTab(spec);
 
         intent = new Intent().setClass(this, Third_Media.class);
-        spec = tabHost.newTabSpec("YouTube").setIndicator("Youtube")
-                .setContent(intent);
+        spec = tabHost.newTabSpec("YouTube")
+                .setIndicator("", getResources().getDrawable(R.drawable.ic_action_youtube))
+                        .setContent(intent);
         tabHost.addTab(spec);
 
         intent = new Intent().setClass(this, Fourth_Media.class);
-        spec = tabHost.newTabSpec("Tumblr").setIndicator("Tumblr")
+        spec = tabHost.newTabSpec("Tumblr")
+                .setIndicator("", getResources().getDrawable(R.drawable.ic_action_tumblr))
                 .setContent(intent);
         tabHost.addTab(spec);
     }
--- app/src/main/java/com/restart/twitchgroup/Third_Media.java
+++ app/src/main/java/com/restart/twitchgroup/Third_Media.java
@@ -2,17 +2,41 @@ package com.restart.twitchgroup;
 
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 
 public class Third_Media extends Activity {
+    private ProgressDialog dialog;
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
+        dialog = ProgressDialog.show(Third_Media.this, "", "Loading. Please wait...", true);
+        webView = (WebView) findViewById(R.id.webView3);
+        webView.setVisibility(View.INVISIBLE);
+        webView.getSettings().setJavaScriptEnabled(true);
 
-        TextView txtView = (TextView) findViewById(R.id.txtDisplayedTab);
-        txtView.setText("First Tab is Selected");
+        webView.setWebViewClient(new WebViewController() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                dialog.dismiss();
+                webView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        webView.loadUrl("https://www.youtube.com/");
+    }
+
+    public class WebViewController extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
